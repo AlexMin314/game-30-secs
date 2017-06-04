074405fc2ada850c4ab21980a0237bd7f3218028
Alex Min <m98stone@gmail.com>
2017-06-04 21:30:09 +0800

    bugfix on lineObj

--- scripts/objLine.js
+++ scripts/objLine.js
@@ -2,7 +2,7 @@
 var LineObj = function (settings, world, dot1, dot2, id) {
 
   var lineLength = 0;
-  var lineDiv = null;
+  var lineDiv = document.getElementById(id);
 
   this.drawLineMove = function () {
 
@@ -13,13 +13,12 @@ var LineObj = function (settings, world, dot1, dot2, id) {
 
     // Calculate angle to rotate line div.
     var calc = Math.atan2(y2 - y1, x2 - x1);
-    calc = calc * 180 / Math.PI;    
+    calc = calc * 180 / Math.PI;
 
     // Line length(distance between the dot1 dot2).
     lineLength = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 
     if (!gameOverChk()) {
-      var lineDiv = document.getElementById(id);
       lineDiv.style.height = '2px';
       lineDiv.style.width = lineLength + 'px';
       lineDiv.style.backgroundColor = 'red';
